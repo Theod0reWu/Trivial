124c48f4e415d80c6a0700aef3351799fe605589
Calvin Chu <cchu00@stuy.edu>
2024-08-10 22:42:46 -0400

    secure meta tag change

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-	backendURL: "https://trivial-wrkfcie6oq-uc.a.run.app",
-	// backendURL: "https://gen-lang-client-0263701804.uc.r.appspot.com"
+  backendURL: 'https://trivial-wrkfcie6oq-uc.a.run.app',
+  secureMetaTag: true,
+  // backendURL: "https://gen-lang-client-0263701804.uc.r.appspot.com"
 };
